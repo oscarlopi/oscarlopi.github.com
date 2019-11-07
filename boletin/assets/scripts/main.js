396b6b311553d5f49d62ea138469043e59b2178f
oscarlopi <oscarlopi@users.noreply.github.com>
2019-11-06 20:18:23 -0600

    Subiendo con boletin razas top de greki

--- boletin/assets/scripts/main.js
+++ boletin/assets/scripts/main.js
@@ -16,7 +16,7 @@ links.forEach(function(link){
     content.classList.add("animated");
 
     setTimeout(function(){
-      location.href = "/boletin";
+      location.href = "../index.html";
     }, 600)
 
   })
